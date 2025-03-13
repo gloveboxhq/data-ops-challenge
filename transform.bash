58109fc08c9aea9ccddf83414e91c4ae97449d1a
mjangda3 <mateenpy@gmail.com>
2025-03-13 06:00:10 +0000

    Mateen Jangda GloveBox Data Ops Challenge
    - Fix permission issue, chmod +x transform.bash.
    - Update to include phone records in users.csv.
    - Use xsv join to combine user and policy CSV data on the user_id field.

--- transform.bash
+++ transform.bash
@@ -1,36 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Extract users data with emails and phones
 jq -rs '
-	[ "user_id", "name", "email", "phone" ],
-	(
-		.[] |
-		.user_id as $id |
-		.name as $name | (
-			( .emails[] | [ $id , $name , .   , null ] ),
-			[ "TODO", "add", "phone", "here" ]
-		)
-	)
-	| @csv
+    [ "user_id", "name", "email", "phone" ],
+    (
+        .[] |
+        .user_id as $id |
+        .name as $name |
+        (
+            (.emails[]? | [ $id, $name, ., null ]),
+            (.phones[]?  | [ $id, $name, null, . ])
+        )
+    )
+    | @csv
 ' tests/input/users/*.json > $BUILD_DIR/users.csv
 
+# Extract policies data
 jq -rs '
-	[ "user_id", "policy_number", "carrier", "policy_type", "effective_date", "expiration_date" ],
-	(
-		.[] | .[] |
-			[
-				.user_id,
-				.policy_number,
-				.carrier,
-				.policy_type,
-				.effective_date,
-				.expiration_date
-			]
-	)
-	| @csv
+    [ "user_id", "policy_number", "carrier", "policy_type", "effective_date", "expiration_date" ],
+    (
+        .[] | .[] |
+            [
+                .user_id,
+                .policy_number,
+                .carrier,
+                .policy_type,
+                .effective_date,
+                .expiration_date
+            ]
+    )
+    | @csv
 ' tests/input/policies-by-user/*.json > $BUILD_DIR/policies.csv
 
-echo "TODO xsv join" | tee $BUILD_DIR/user-policies.csv
-
-
-
+# Join them on user_id
+xsv join user_id $BUILD_DIR/policies.csv user_id $BUILD_DIR/users.csv > $BUILD_DIR/user-policies.csv
